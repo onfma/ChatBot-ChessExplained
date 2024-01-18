b1b31faeafc69d72c4fc3c531a06ffa5227fcf4c
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2024-01-18 10:55:04 +0200

    update

--- bot.py
+++ bot.py
@@ -1,15 +1,26 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 from cleaner import clean_corpus
+import collections.abc
+collections.Hashable = collections.abc.Hashable
 
 
 CORPUS_FILE = "chat.txt"
 
 chatbot = ChatBot("ChessBot")
 
-trainer = ListTrainer(chatbot)
+# Crearea unui antrenor pentru chatbot
+corpus_trainer = ChatterBotCorpusTrainer(chatbot)
+list_trainer = ListTrainer(chatbot)
+
+# Antrenarea chatbotului pe baza corpusului în limba engleză
+corpus_trainer.train("chatterbot.corpus.english")
+
 cleaned_corpus = clean_corpus(CORPUS_FILE)
-trainer.train(cleaned_corpus)
+
+# Antrenarea chatbotului pe fiecare conversație din cleaned_corpus
+for conversation in cleaned_corpus:
+    list_trainer.train(conversation)
 
 exit_conditions = (":q", "quit", "exit")
 while True:
